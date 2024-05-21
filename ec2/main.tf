b6049c3117140eff6ba5bd0c2690da5748786502
ejwoods <ewoods08@gmail.com>
2024-05-21 15:44:43 -0400

    terraform init ec2 dir
    - create ec2/main.tf
    - run `terraform init`

---
+++ ec2/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example_server" {
+  ami = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-sf-ec2-001"
+  }
+}
