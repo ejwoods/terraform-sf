a21ac79cca848dae93afc1b7c2255a119c7cacf7
ejwoods <ewoods08@gmail.com>
2024-05-21 16:07:34 -0400

    rename ec2 instance in main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example_server" {
+resource "aws_instance" "terraform-sf-ec2-001" {
   ami = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
   tags = {
