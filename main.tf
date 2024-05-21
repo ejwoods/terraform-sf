fa302d4d8b47db2dd0994252bd22c3d33448e378
ejwoods <ewoods08@gmail.com>
2024-05-21 14:31:38 -0400

    update main

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_s3_bucket" "my_bucket" {
-  bucket  = "my-unique-bucket-name"
+  bucket  = "terraform-sf-s3-001"
   tags    = {
 	Name          = "MyS3Bucket"
 	Environment    = "Production"
